dd7d85a6e621540197a2094bbbd3b461b2440e4c
Nasar Eddaoui <nasar165@hotmail.com>
2024-05-03 23:29:46 +0700

    final push

--- app/components/socket.tsx
+++ app/components/socket.tsx
@@ -2,16 +2,19 @@
 
 import React, { useEffect, useState } from 'react';
 import { Websocket } from '../lib/websocket/socket';
+import { CloseEvent, ISocket } from '../model/websocket';
 
 type Url = { url: string };
 
 export default function Socket({ url }: Url): React.JSX.Element {
-  const [socket, setSocket] = useState<Websocket>();
+  const [socket, setSocket] = useState<ISocket>();
+
+  const closeEvent: CloseEvent = (code: number) => console.log(code);
 
   const start = () => {
     try {
       if (!socket?.Alive()) {
-        socket?.Start(url);
+        socket?.Start(url, closeEvent);
         setSocket(socket);
       } else socket?.Stop(1000);
     } catch (err) {
--- app/model/websocket.ts
+++ app/model/websocket.ts
@@ -1,11 +1,13 @@
 const LIVE = 'web socket client is already initialized';
 const DEAD = 'web socket client is dead, open a new connection';
 
+type CloseEvent = (code: number) => void;
+
 interface ISocket {
   Alive(): boolean;
   Start(url: string, event: CloseEvent): void;
   Stop(code: number): void;
 }
 
-export type { ISocket };
+export type { ISocket, CloseEvent };
 export { LIVE, DEAD };
