b5c9b524d1e398ebc17d37660293b58ec9bf4d2a
Nasar Eddaoui <nasar165@hotmail.com>
2024-05-04 00:16:39 +0700

    final push

--- app/lib/websocket/socket.ts
+++ app/lib/websocket/socket.ts
@@ -17,19 +17,53 @@ export class Websocket implements ISocket {
 
     this.socket = new WebSocket(url, ['ocpp1.6']);
     this.stateChange(true);
+    this.setEventListeners();
+
     console.info('Websocket connection was successful');
   }
 
   Stop(code?: number): void {
-    if (!this.Alive()) throw new Error(DEAD);
-    this.socket!.close(code ?? 1000);
-    this.stateChange(false);
+    if (this.socket == null) throw new Error(DEAD);
+
+    try {
+      this.stateChange(false);
+      this.socket!.close(code ?? 1000);
+    } catch (err) {
+      throw err;
+    } finally {
+      this.removeEventListeners();
+    }
+
     console.info('socket closed');
+  }
+
+  protected setEventListeners(): void {
+    this.socket!.addEventListener('close', this.close.bind(this));
+    this.socket!.addEventListener('error', this.error.bind(this));
+    this.socket!.addEventListener('open', this.open.bind(this));
+  }
+
+  protected removeEventListeners(): void {
+    this.socket!.removeEventListener('close', this.close.bind(this));
+    this.socket!.removeEventListener('error', this.error.bind(this));
+    this.socket!.addEventListener('open', this.open.bind(this));
+  }
+
+  protected close(reason: CloseEvent): void {
+    console.info(reason);
+    this.Stop(1000);
+  }
+
+  protected error(ev: Event): void {
+    console.error('an error has occurred', ev);
+  }
+
+  protected open(): void {
+    this.stateChange(true);
   }
 
   Send(message: string) {
     if (!this.Alive()) throw new Error(DEAD);
-
     this.socket!.send(message);
   }
 }
