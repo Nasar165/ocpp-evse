6d33297187758fa8b33a1d8e7c9c673cd267756f
Nasar Eddaoui <nasar165@hotmail.com>
2024-05-03 20:04:06 +0700

    add next.js to project

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Home() {
+  return <div className='bg-red-600 text-center'>Hello world</div>;
+}
---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  poweredByHeader: false,
+  compress: true,
+};
+
+export default nextConfig;
